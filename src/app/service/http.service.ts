073c338e562c6874aa82f470be35ccada07b3577
VesterDe <demjan.vester@gmail.com>
2022-12-20 18:01:43 +0100

    correctl implement pagination

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -4,39 +4,33 @@ import { Observable } from 'rxjs';
 import { Dog } from '../interface/dog';
 import { DogInfo } from '../interface/dogInfo';
 
-
-let DOGS_FETCHED = 0;
-const DOGS_TO_FETCH = 15
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
-
   // url: string = "assets/dogs.json";
 
   httpHeaders = new HttpHeaders({
-    Authorization: "live_yOnbrVsZCcsffeeisScz8EByvFsHEtIubmS7wtwC8jyfu0KNQzBDjA8ZhpBikYw9"
-  })
+    Authorization:
+      'live_yOnbrVsZCcsffeeisScz8EByvFsHEtIubmS7wtwC8jyfu0KNQzBDjA8ZhpBikYw9',
+  });
 
-  numberOfDogs:number = 15;
-  page = (DOGS_FETCHED + this.numberOfDogs) / this.numberOfDogs - 1;
+  DOGS_FETCHED = 0;
+  DOGS_TO_FETCH = 15;
 
-  url: string =  `https://api.thedogapi.com/v1/breeds?page=${this.page}&order=desc&limit=${this.numberOfDogs}`;
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  fetchDogsFromApi(): Observable<DogInfo[]> {
+    const page =
+      (this.DOGS_FETCHED + this.DOGS_TO_FETCH) / this.DOGS_TO_FETCH - 1;
 
-  fetchDogsFromApi(): Observable<Dog[]> {
-    return this.http.get<Dog[]>(this.url, {headers: this.httpHeaders});
+    const url = `https://api.thedogapi.com/v1/breeds?page=${page}&order=desc&limit=${this.DOGS_TO_FETCH}`;
+
+    return this.http
+      .get<Dog[]>(url, { headers: this.httpHeaders })
+      .pipe((response) => {
+        this.DOGS_FETCHED += this.DOGS_TO_FETCH;
+        return response;
+      });
   }
-
-  fetchDogInfoFromApi(): Observable<DogInfo[]> {
-    return this.http.get<DogInfo[]>(this.url, {headers: this.httpHeaders});
-  }
-
-  // mapToDogs(): Observable<Dog[]> {
-  // return this.http.get<Dog[]>(this.url, {headers: this.httpHeaders}).pipe(
-  //     map((dog: Dog[]) => dog.map(pes => pes.url))
-  //   );
-  // }
 }
